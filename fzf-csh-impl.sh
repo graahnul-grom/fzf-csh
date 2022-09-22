9e10ce27284d4114b8bf1be294e3a552e533b0c6
dmn <graahnul.grom@ya.ru>
2022-09-22 22:44:31 +0300

    impl sh: set -o pipefail: pipeline returns !0 on Esc press

--- fzf-csh-impl.sh
+++ fzf-csh-impl.sh
@@ -12,6 +12,8 @@ KEY_2="^X^F^G^H^I^J"
 echo -n bindkey -s \"${KEY_2}\" > $FILE_CMD
 echo -n " " >> $FILE_CMD
 
+set -o pipefail
+
 fzf --tac --no-sort < $FILE_HIS | \
     sed -e 's,\\,\\\\\\\\,g' | \
     sed -e 's, ,\\ ,g'  | \
@@ -36,13 +38,7 @@ fzf --tac --no-sort < $FILE_HIS | \
     sed -e 's,\^,\\\\^,g' \
     >> $FILE_CMD
 
-# TODO:
-# csh: cat LINES | fzf | sed -e 's,1,Y,' => press Esc => $? == 130
-# sh:  cat LINES | fzf | sed -e 's,1,Y,' => press Esc => $? == 0
-ret=$?
-echo ".. [$ret]"
-
-if [ $ret -ne 0 ]; then
+if [ $? -ne 0 ]; then
     echo bindkey \"${KEY_2}\" backward-char > $FILE_CMD
 fi
 
